0d8354ca22ae77115e12c2fefeeb6681008b7fd1
Alexandre S <moutardea@gmail.com>
2022-07-09 11:56:54 +0200

    Ajout d'un bouton sur la modal quand un lien est présent

--- src/components/ProjectDialog.vue
+++ src/components/ProjectDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog width="50%" v-model="props.active">
+  <v-dialog width="50%" :model-value="props.active">
     <v-card>
       <v-card-title class="border-b">
         {{ props.project.name }}
@@ -18,7 +18,9 @@
         </div>
       </v-card-text>
       <v-card-actions>
-        <v-btn color="primary" block @click="emits('close-dialog')">Fermer</v-btn>
+        <v-btn @click="emits('close-dialog')">Fermer</v-btn>
+        <v-spacer />
+        <v-btn color="primary" :href="props.project.link" target="_blank" v-if="props.project.link">Visiter le site</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
